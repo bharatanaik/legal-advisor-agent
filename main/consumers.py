987fe4533c96e3c877364b1c29c32c6cf80d561f
bharatanaik <bharat.anaik2003@gmail.com>
2024-10-16 13:01:29 +0530

    ui update

--- main/consumers.py
+++ main/consumers.py
@@ -1,4 +1,5 @@
 import json
+import time
 from channels.generic.websocket import WebsocketConsumer
 from core.settings import app
 from main.agent import query
@@ -7,6 +8,10 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.accept()
+        self.send(text_data=json.dumps({
+            "message":"Hello, I am your legal advisor agent /n please feel to ask about law",
+            "checkpoint":"beginner-1"
+        }))
 
     def disconnect(self, close_code):
         pass
@@ -14,5 +19,5 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        for chunk in query(app, self.room_name, message):
-            self.send(text_data=json.dumps({"message": chunk}))
+        for chunk, checkpoint, is_stop in query(app, self.room_name, message):
+            self.send(text_data=json.dumps({"message": chunk, "checkpoint":checkpoint, "is_stop":is_stop}))
